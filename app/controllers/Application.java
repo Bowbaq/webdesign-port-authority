955763c898e0f9a15160f9202831e09b30ac251c
Maxime Bury <maxime.bury@gmail.com>
2012-12-05 12:13:33 -0500

    Bringing project up to date
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,116 +1,11 @@
 package controllers;
 
-import com.avaje.ebean.Ebean;
-import models.User;
-import org.mindrot.jbcrypt.BCrypt;
-import play.data.Form;
-import play.data.validation.Constraints;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.login;
-import views.html.register;
 
 public class Application extends Controller {
-    public static final String FLASH_MESSAGE = "message";
-    public static final String FLASH_ERROR = "error";
-
-    public static Result login() {
-        if(session().get("email") != null) {
-            return redirect(
-                    routes.Transit.favorites()
-            );
-        }
-
-        return ok(
-            login.render(form(Login.class))
-        );
-    }
     
-    public static Result register() {
-        if(session().get("email") != null) {
-            return redirect(
-                routes.Transit.favorites()
-            );
-        }
-
-        return ok(
-            register.render(form(Register.class))
-        );
-    }
-    
-    public static Result newuser() {
-        User u;
-        final Form<Register> form = form(Register.class).bindFromRequest();
-        Ebean.beginTransaction();
-        try {
-            u = User.findByEmail(form.field("email").value());
-            if(null != u) {
-                flash(FLASH_ERROR, "A user with the same email address already exists");
-                Ebean.rollbackTransaction();
-                return badRequest(register.render(form));
-            }
-
-            u = new User();
-            u.email = form.field("email").value();
-            u.password = BCrypt.hashpw(form.field("password").value(), BCrypt.gensalt());
-            u.save();
-            Ebean.commitTransaction();
-        } catch (Exception e) {
-            flash(FLASH_ERROR, "An error occurred during the registration process, please try again");
-            Ebean.rollbackTransaction();
-            return badRequest(register.render(form));
-        } finally {
-            Ebean.endTransaction();
-        }
-
-        session("email", u.email);
-        return redirect(
-                routes.Transit.favorites()
-        );
-    }
-
-    public static Result authenticate() {
-        final Form<Login> form = form(Login.class).bindFromRequest();
-        if(form.hasErrors()) {
-            return badRequest(login.render(form));
-        } else {
-            session("email", form.field("email").value());
-            return redirect(
-                routes.Transit.favorites()
-            );
-        }
-    }
-
-    public static Result logout() {
-        session().clear();
-        flash(FLASH_MESSAGE, "You've been logged out");
-        return redirect(
-                routes.Application.login()
-        );
-    }
-
-
-    public static class Login {
-
-        public String email;
-        public String password;
-
-        public String validate() {
-            if(User.authenticate(email, password) == null) {
-                return "Invalid user or password";
-            }
-            return null;
-        }
-
-    }
-    
-    public static class Register {
-        @Constraints.Required
-        @Constraints.Email
-        public String email;
-
-        @Constraints.Required
-        @Constraints.MinLength(8)
-        public String password;
+    public static Result index() {
+        return ok("");
     }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,16 +8,21 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "mysql" % "mysql-connector-java" % "5.1.18"
+        "mysql" % "mysql-connector-java" % "5.1.18",
+        
+        // Apache DateUtils
+        "org.apache.commons" % "commons-lang3" % "3.1"
     )
     
-    // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory 
-    
     def customLessEntryPoints(base: File): PathFinder =
-        (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++ 
-        (base / "app" / "assets" / "stylesheets" ** "style.less") 
+        (base / "app" / "assets" / "stylesheets" / "vendor" / "bootstrap"  * "bootstrap.less") +++ 
+        (base / "app" / "assets" / "stylesheets" / "vendor" / "bootstrap"  * "responsive.less") +++ 
+        (base / "app" / "assets" / "stylesheets" ** "style.less")
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-        lessEntryPoints <<= baseDirectory(customLessEntryPoints)
+        // Less assets compilation
+        lessEntryPoints <<= baseDirectory(customLessEntryPoints),
+
+        routesImport += "util.DoubleW._"
     )
 }
