7ee826df7c7b9d937d5a6c59138125ed89fadb66
Maxime Bury <maxime.bury@gmail.com>
2012-12-08 16:24:20 -0500

    Stylin' in progress
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,11 +2,11 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import views.html.transit.schedule;
 
 public class Application extends Controller {
     
     public static Result index() {
-        return ok(index.render());
+        return ok(schedule.render());
     }
 }
