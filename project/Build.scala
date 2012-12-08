330d829ea8c3df00fbc9cae57eeb21f87b7ac938
Maxime Bury <maxime.bury@gmail.com>
2012-12-08 14:02:29 -0500

    WIP basic layout
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,25 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "PortAuthority"
+    val appName         = "PbghTransit"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "mysql" % "mysql-connector-java" % "5.1.18"
+        // Mysql driver
+        "mysql" % "mysql-connector-java" % "5.1.18",
+        // Apache DateUtils
+        "org.apache.commons" % "commons-lang3" % "3.1"
     )
+
+    def customLessEntryPoints(base: File): PathFinder =
+        (base / "app" / "assets" / "stylesheets" / "vendor" / "bootstrap"  * "bootstrap.less") +++ 
+        (base / "app" / "assets" / "stylesheets" / "vendor" / "bootstrap"  * "responsive.less") +++ 
+        (base / "app" / "assets" / "stylesheets" ** "style.less")
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+        // Less assets compilation
+        lessEntryPoints <<= baseDirectory(customLessEntryPoints),
 
+        routesImport += "util.DoubleW._"
+    )
 }
