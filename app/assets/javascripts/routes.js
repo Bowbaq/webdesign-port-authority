4416060d56c06f47a928cac556fd9da37eeedf96
Maxime Bury <maxime.bury@gmail.com>
2012-12-10 11:07:17 -0500

    Fixed the damn thing for good this time
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,9 +1,10 @@
 var Routes = (function(routes) {
   var template = _.template($('#routes-form').html()),
-      form
+      form,
+      FORM_TRANSITION = 500
   ;
   
-  var lastOfLine = function lastOfLine($this) {
+  function lastOfLine($this) {
       var ref_top = $this.position().top,
           prev = $this,
           next = $this.next('.route')
@@ -17,57 +18,68 @@ var Routes = (function(routes) {
       return prev;
   };
   
-  var preHideForm = function preHideForm() {
+  function preHideForm() {
     var $this = $(this);    
     
+    hideForm(function(){
+      $this.off('click.hide');
+      $this.on('click.show', showForm);
+    });
+  };
+  
+  function hideForm(callback) {
     if(form !== undefined && form !== null) {
       setTimeout(function(){
         form.removeClass('expanded');
       }, 0);
       
       setTimeout(function(){
-        hideForm();
-        $this.removeClass('selected');
-      }, 500);
-    }
-    
-    $this.off('click.hide');
-    $this.on('click.show', showForm);
-  };
-  
-  var hideForm = function hideForm() {
-    if(form !== undefined && form !== null) {
         form.remove();
+        form = null;
+        
+        // Remove the arrow from the route
+        $('.route').removeClass('selected');
+        
+        callback();
+      }, FORM_TRANSITION);
+    } else {
+      callback();
     }
   };
   
-  var showForm = function showForm() {
+  function showForm() {
     var $this = $(this);
     
-    hideForm();
-    form = $(template({}));
-    lastOfLine($this).after(form);
-    
-    $this.addClass('selected');
-    $this.off('click.show');
-    $this.on('click.hide', preHideForm);
-    form.find('[name="direction"]').on('click.direction', function(){
-      var $radio = $(this),
-          $select = $radio.nextAll('select[name="stop"]'),
-          $go = $radio.nextAll('input[type="submit"]')
-      ;
-      
-      $select.html($this.find('script.inbound').html());
-      $go.on('click', function(e){
-        e.preventDefault();
-        alert('This doesn\'t work yet ...');
+    // Hide any previously displayed form
+    hideForm(function(){
+      // Fill the new form with the template
+      form = $(template({}));
+
+      // Find the last element of the line 
+      lastOfLine($this).after(form);
+
+      $this.addClass('selected');
+      $this.off('click.show');
+      $this.on('click.hide', preHideForm);
+
+      form.find('[name="direction"]').on('click.direction', function(){
+        var $radio = $(this),
+            $select = $radio.nextAll('select[name="stop"]'),
+            $go = $radio.nextAll('input[type="submit"]')
+        ;
+
+        $select.html($this.find('script.inbound').html());
+        $go.on('click', function(e){
+          e.preventDefault();
+          alert('This doesn\'t work yet ...');
+        });
+        $go.removeAttr('disabled');
       });
-      $go.removeAttr('disabled');
+
+      setTimeout(function(){
+        form.addClass('expanded');
+      }, 0);
     });
-    
-    setTimeout(function(){
-      form.addClass('expanded');
-    }, 0);
   };
   
   routes.hook = function hook() {
