676f738b84fb372cd81eb44eb8e54dc921bfe251
Maxime Bury <maxime.bury@gmail.com>
2012-12-11 18:01:53 -0500

    Big perf improvement
    Signed-off-by: Maxime Bury <maxime.bury@gmail.com>

--- app/models/gtfs/Route.java
+++ app/models/gtfs/Route.java
@@ -1,13 +1,16 @@
 package models.gtfs;
 
-import com.avaje.ebean.*;
-import play.Logger;
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlQuery;
+import com.avaje.ebean.SqlRow;
 import play.cache.Cache;
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Entity
@@ -29,7 +32,76 @@ public class Route extends Model {
 
     private Directions directions;
 
-    public List<Stop> stops(Integer direction) {
+    private List<Stop> inbound;
+    private List<Stop> outbound;
+
+    public Route() {
+        directions = new Directions();
+        inbound = new ArrayList<Stop>();
+        outbound = new ArrayList<Stop>();
+    }
+
+    public static ArrayList<Route> getAllWithStops() {
+        ArrayList<Route> routes = (ArrayList<Route>) Cache.get("route_stops");
+
+        if(routes == null) {
+            HashMap<String, Route> tmp_routes = new HashMap<String, Route>();
+
+            String sql = "SELECT route_id, route_short_name, route_long_name, route_type, route_direction, "
+                    + "stop_id, stop_code, stop_name, stop_lat, stop_lng, stop_zone "
+                    + "FROM route_stops ORDER BY route_id ASC, route_direction ASC, stop_id ASC";
+
+            SqlQuery query = Ebean.createSqlQuery(sql);
+
+            List<SqlRow> result = query.findList();
+
+            for(SqlRow row : result) {
+                String route_id = row.getString("route_id");
+                Route route = tmp_routes.get(route_id);
+                if(route == null) {
+                    route = new Route();
+                    route.id = route_id;
+                    route.short_name = row.getString("route_short_name");
+                    route.long_name = row.getString("route_long_name");
+                    route.type = row.getInteger("route_type");
+
+                    tmp_routes.put(route_id, route);
+                }
+
+                Integer direction = row.getInteger("route_direction");
+                route.directions.add(direction);
+
+                Stop stop = new Stop();
+                stop.id = row.getString("stop_id");
+                stop.code = row.getString("stop_code");
+                stop.name = row.getString("stop_name");
+                stop.lat = row.getDouble("stop_lat");
+                stop.lng = row.getDouble("stop_lng");
+                stop.zone = row.getString("stop_zone");
+
+                if(direction == Trip.INBOUND) {
+                    route.inbound.add(stop);
+                } else {
+                    route.outbound.add(stop);
+                }
+            }
+
+            routes = new ArrayList<Route>(tmp_routes.values());
+            Cache.set("route_stops", routes);
+        }
+
+        return routes;
+    }
+
+    public List<Stop> inboundStops() {
+        return this.inbound;
+    }
+
+    public List<Stop> outboundStops() {
+        return this.outbound;
+    }
+
+/*    public List<Stop> stops(Integer direction) {
         List<Stop> stops = (List<Stop>) Cache.get("route_stops_" + id + "_" + direction);
         if(stops == null ) {
             Logger.info("Cache miss : " + "route_stops_" + id + "_" + direction);
@@ -61,7 +133,7 @@ public class Route extends Model {
         }
 
         return stops;
-    }
+    }*/
     
     public Directions directions() {
         if (null == directions) {
@@ -72,7 +144,6 @@ public class Route extends Model {
             SqlQuery query = Ebean.createSqlQuery(sql);
             query.setParameter("id", id);
 
-            // execute the query returning a List of MapBean objects
             List<SqlRow> result = query.findList();
 
             for(SqlRow row : result) {
