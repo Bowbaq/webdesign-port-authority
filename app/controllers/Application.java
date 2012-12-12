9b305c8c51bac35c95a15a58d4a25d62980e9320
Junki <junkinakayama@gmail.com>
2012-12-11 19:42:50 -0500

    WIP static pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,7 +5,10 @@ import models.gtfs.Stop;
 import org.codehaus.jackson.map.ObjectMapper;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.transit.schedule;
+import views.html.transit.*;
+import views.html.fares.*;
+import views.html.service.*;
+import views.html.info.*;
 
 import java.io.IOException;
 import java.util.Date;
@@ -30,5 +33,31 @@ public class Application extends Controller {
                 route_id,
                 direction
         )));
+    }
+
+    public static Result fares(){
+        return ok(fares.render());
+    }
+
+    public static Result zones(){
+        return ok(zones.render());
+    }
+    /*public static Result map(){
+        return ok(map.render());
+    }*/
+    public static Result buy(){
+        return ok(buy.render());
+    }
+    public static Result info(){
+        return ok(info.render());
+    }
+    /*public static Result changes(){
+        return ok(changes.render());
+    }*/
+    public static Result service(){
+        return ok(service.render());
+    }
+    public static Result special(){
+        return ok(special.render());
     }
 }
